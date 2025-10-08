920d703008d0755022e0e96599480243292369ef
Francesco <77879644+VampaFrancesco@users.noreply.github.com>
2025-10-08 14:20:06 +0200

    Enable Feign client for weather codes API
    Added @EnableFeignClients to the main application class and implemented a Feign client to fetch weather codes from a remote API. Configured the remote API URL in application.properties.

--- src/main/java/tech/aesys/finale/routine/RoutineApiServiceApplication.java
+++ src/main/java/tech/aesys/finale/routine/RoutineApiServiceApplication.java
@@ -2,8 +2,10 @@ package tech.aesys.finale.routine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class RoutineApiServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/tech/aesys/finale/routine/client/RoutineFeignClient.java
+++ src/main/java/tech/aesys/finale/routine/client/RoutineFeignClient.java
@@ -2,13 +2,21 @@ package tech.aesys.finale.routine.client;
 
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import tech.aesys.finale.routine.client.config.RoutineFeignConfig;
+import tech.aesys.finale.routine.model.WeatherCode;
+
+import java.util.List;
 
 
 @FeignClient(name = "RoutineFeignClient",
-        url = "",
+        url = "${weather.codes.remote}",
         configuration = RoutineFeignConfig.class
 )
 public interface RoutineFeignClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/weather/codes")
+    List<WeatherCode> getAllWeatherCodes();
 
 }
